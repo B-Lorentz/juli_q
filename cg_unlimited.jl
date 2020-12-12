7369313efb79972f94abfe2578803bec2bafb5d7
L Bódy <borenz1@hotmail.com>
2020-12-12 12:31:22 +0100

    full decomp

--- cg_unlimited.jl
+++ cg_unlimited.jl
@@ -53,18 +53,19 @@ function check_decomposition(j1::Rational, j2::Rational, J::Rational, M::Rationa
     close(io)
 end
 
-full_prob(k::LinKet) = sum(k.coeffs .^2)
+full_prob(k::LinKet) = sum(alg2f.( k.coeffs) .^2)
 
 function full_decompose(js, J::Rational, M::Rational)
     if length(js) == 2
         return decompose(js[1], js[2], J, M, 0, 1)
     else
-        jrems = (J-js[end]):(J+js[end])
-        result = ZeroKet()
-        for jrem in jrems
-
+        jrems = (abs(J-js[end])):(J+js[end])
+        results = ZeroKet()
+        sum = 0
+        for jrem in reverse(jrems)
+            result = ZeroKet()
             bin = decompose(jrem, js[end], J,M, 0, length(js)-1)
-            println(full_prob(bin))
+        #    println(full_prob(bin))
             for c_k = zip(bin.coeffs, bin.base_kets)
                 c, k = c_k
                 try
@@ -77,12 +78,62 @@ function full_decompose(js, J::Rational, M::Rational)
                     end
                 end
             end
-            println()
+        #    println()
+            results += result
+            if result != ZeroKet()
+                return result
+            end
         end
-        return result
+    #    return (1/sqrt(AlgebraicNumber(sum)))*results
     end
 end
-println(full_prob(full_decompose([1//1, 2//1, 1//1], 3//1, 2//1 )))
+
+function all_Js(js)
+    if length(js) == 2
+        return abs(js[1]-js[2]):(js[1] + js[2])
+    else
+        Jpos = all_Js(js[1:end-1])
+        allJ =  []
+        for J in Jpos
+            allJ = vcat(allJ, (abs(J-js[end]):(J + js[end])))
+        end
+        return sort(unique(allJ))
+    end
+end
+
+function great_check(js)
+    Js = all_Js(js)
+    decons = []
+    fulls = []
+    for J in Js
+        for M in -J:J
+            push!()
+            push!(decons, full_decompose(js, J,M ))
+        end
+    end
+    decons
+end
+
+function great_check2(js)
+    Js = all_Js(js)
+    decons = []
+    fulls = []
+    J=max(Js)
+    for M in -J:J
+        push!()
+        push!(decons, full_decompose(js, J,M ))
+    end
+    decons
+end
+
+#println(great_check([1//1, 1//1,1//1]))
+res = full_decompose( [1//1, 1//1, 1//1], 1//1, 1//1 )
+println(res)
+println(full_prob(res))
+#println(decompose(2//1, 1//1, 2//1, 2//1))
+#println(JJdecomp(1//1, 1//1, 0//1))
+#println(all_Js([3, 13, 5]))
+#great_check([1//1,1//1,1//1])
 #check_decomposition(8 + 5//2, 3//2, 8//1, 8//1, "de_check.py")
 #println(apply_op(jK(5, 4, 0)⊗jK(3//2, -1//2, 1), _J_d))
 #println(apply_op(jK(5, 5, 0)⊗jK(3//2, -3//2, 1), _J_d))
